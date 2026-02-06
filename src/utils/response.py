8a218beffdb1156c6b836f9028c0bd2baab5e843
Eren Arda Çolak <ea.colak0@gmail.com>
2026-02-06 11:18:05 +0300

    Ufak güncellemeler.

--- src/utils/response.py
+++ src/utils/response.py
@@ -1,13 +1,35 @@
 from sdks.novavision.src.helper.package import PackageHelper
-from components.FileSave.src.models.PackageModel import OutputText, FileSaveOutputs, FileSaveResponse, FileSaveExecutor, ConfigExecutor, PackageConfigs, PackageModel
+from components.FileSave.src.models.PackageModel import (
+    PackageModel, PackageConfigs, ConfigExecutor,
+    ImageSave, ImageResponse, ImageOutputs,
+    CSVSave, CSVResponse, CSVOutputs,
+    OutputText
+)
 
-def build_response(context):
-    outputText = OutputText(value=context.output_message)
-    fileSaveOutputs = FileSaveOutputs(outputText=outputText)
-    imageSaveOutputs = FileSaveResponse(outputs=fileSaveOutputs)
-    imageSaveExecutor = FileSaveExecutor(value=imageSaveOutputs)
-    executor = ConfigExecutor(value=imageSaveExecutor)
-    packageConfigs = PackageConfigs(executor=executor)
-    package = PackageHelper(packageModel=PackageModel, packageConfigs=packageConfigs)
-    packageModel = package.build_model(context)
-    return packageModel
+
+def build_image_save_response(context):
+    output_text = OutputText(value=context.output_message)
+    image_outputs = ImageOutputs(outputText=output_text)
+    image_response = ImageResponse(outputs=image_outputs)
+    image_executor = ImageSave(value=image_response)
+    config_executor = ConfigExecutor(value=image_executor)
+    package_configs = PackageConfigs(executor=config_executor)
+    package = PackageHelper(
+        packageModel=PackageModel,
+        packageConfigs=package_configs
+    )
+    return package.build_model(context)
+
+
+def build_csv_save_response(context):
+    output_text = OutputText(value=context.output_message)
+    csv_outputs = CSVOutputs(outputText=output_text)
+    csv_response = CSVResponse(outputs=csv_outputs)
+    csv_executor = CSVSave(value=csv_response)
+    config_executor = ConfigExecutor(value=csv_executor)
+    package_configs = PackageConfigs(executor=config_executor)
+    package = PackageHelper(
+        packageModel=PackageModel,
+        packageConfigs=package_configs
+    )
+    return package.build_model(context)
